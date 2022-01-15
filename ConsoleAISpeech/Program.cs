a67a3c9143998a30786ce6809802bd37cd67485f
RicardoJosuePerezAltamirano <43554761+RicardoJosuePerezAltamirano@users.noreply.github.com>
2022-01-15 10:15:11 -0600

    clean keys

--- ConsoleAISpeech/Program.cs
+++ ConsoleAISpeech/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleAISpeech
     {
         static async Task Main(string[] args)
         {
-            var sppechConfig = SpeechConfig.FromEndpoint(new Uri("https://southcentralus.api.cognitive.microsoft.com/sts/v1.0/issuetoken"), "f943293eeeeb442b965c469afb3155f9");
+            var sppechConfig = SpeechConfig.FromEndpoint(new Uri("https://southcentralus.api.cognitive.microsoft.com/sts/v1.0/issuetoken"), "");
             sppechConfig.SpeechRecognitionLanguage = "es-MX";
             using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
             using var recognizer = new SpeechRecognizer(sppechConfig, audioConfig);
